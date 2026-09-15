de6686a0f1cd137e503eabd90289d1702a94c4dd
jlopezbascoy <jlopezbascoy@gmail.com>
2026-09-15 09:07:54 +0200

    Commit de prueba

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -67,7 +67,7 @@ public class GamePanel extends JPanel implements Runnable{
             }
 
             if(timer>1000000000){
-                System.out.println("FPS:" + drawCount);
+                System.out.println("FPS prueba:" + drawCount);
             }
 
         }
